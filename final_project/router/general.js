e3c1ee609eab52d1b80c49278369c925bf326053
Zhaoyu <zhaoyu.guo@mail.thecustomercompany.ca>
2023-07-19 18:32:38 -0400

    TASK 10, 11, 12 AND 13 Completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,11 +33,53 @@ public_users.get("/", function (req, res) {
   return res.status(200).json({ books });
 });
 
+// TASK 10: available books
+public_users.get("/books", async function (req, res) {
+  return await new Promise((resolve, reject) => {
+    resolve(res.status(200).json({ books }));
+  }).then((result) => {
+    console.log("Task 10 promise resolved");
+  });
+});
+
+// TASK 11: book details via ISBN
+public_users.get("books/isbn/:isbn", async function (req, res) {
+  const callback = new Promise((resolve, reject) => {
+    const ISBN = req.params.isbn;
+    resolve(res.status(200).json({ ...books[ISBN] }));
+  });
+
+  return await callback.then((result) => {
+    console.log("Task 11 promise resolved");
+  });
+});
+
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   const ISBN = req.params.isbn;
 
   return res.status(200).json({ ...books[ISBN] });
+});
+
+// TASK 12: book details via author
+public_users.get("/books/author/:author", async function (req, res) {
+  return await new Promise((resolve, reject) => {
+    const author = req.params.author;
+
+    // find the book by the author's name
+    const allBooks = Object.values(books).filter(
+      (book) => book.author === author
+    );
+
+    // if no book is found
+    if (!allBooks) {
+      reject(res.status(404).json({ message: "Book not found" }));
+    }
+
+    resolve(res.status(200).json({ booksByAuthor: allBooks }));
+  }).then((result) => {
+    console.log("Task 12 promise resolved");
+  });
 });
 
 // Get book details based on author
@@ -55,6 +97,27 @@ public_users.get("/author/:author", function (req, res) {
 
   return res.status(200).json({
     booksByAuthor: allBooks,
+  });
+});
+
+// TASK 13: book details via title
+public_users.get("/books/title/:title", async function (req, res) {
+  return await new Promise((resolve, reject) => {
+    const title = req.params.title;
+
+    // find the book by the title's name
+    const allBooks = Object.values(books).filter(
+      (book) => book.title === title
+    );
+
+    // if no book is found
+    if (!allBooks) {
+      reject(res.status(404).json({ message: "Book not found" }));
+    }
+
+    resolve(res.status(200).json({ booksByTitle: allBooks }));
+  }).then((result) => {
+    console.log("Task 13 promise resolved");
   });
 });
 
